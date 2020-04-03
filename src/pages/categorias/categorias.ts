5e15ac335d7d9247126bb1673f27e507eefff803
Rodrigo marinho <rodrigo@pontta.com>
2020-04-03 11:21:18 -0300

    117. Listando Categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { CategoriaService } from "../../services/domain/categoria.servise";
 
 @IonicPage()
 @Component({
-  selector: 'page-categorias',
-  templateUrl: 'categorias.html',
+  selector: "page-categorias",
+  templateUrl: "categorias.html"
 })
 export class CategoriasPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public categoriaService: CategoriaService
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
+    this.categoriaService.findAll().subscribe(
+      response => {
+        console.log(response);
+      },
+      error => {
+        console.log(console.error);
+      }
+    );
   }
-
 }
