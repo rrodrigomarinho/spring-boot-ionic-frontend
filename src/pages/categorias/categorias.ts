7a8c4e1f51865356ec4d33a2c278a2e1e6ad5506
Rodrigo marinho <rodrigo@pontta.com>
2020-04-04 12:01:29 -0300

    118 - Mostrando as categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { CategoriaService } from "../../services/domain/categoria.servise";
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -8,6 +10,10 @@ import { CategoriaService } from "../../services/domain/categoria.servise";
   templateUrl: "categorias.html"
 })
 export class CategoriasPage {
+
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+  items: CategoriaDTO[];
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -17,7 +23,7 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll().subscribe(
       response => {
-        console.log(response);
+        this.items = response;
       },
       error => {
         console.log(console.error);
