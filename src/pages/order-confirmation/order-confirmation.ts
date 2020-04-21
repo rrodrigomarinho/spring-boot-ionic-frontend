d51a45263c6c31bd3f9d878afd4ea4cb20d1c460
Rodrigo marinho <rodrigo@pontta.com>
2020-04-21 10:52:56 -0300

    152. Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -27,7 +28,7 @@ export class OrderConfirmationPage {
     public clienteService: ClienteService,
     public pedidoService: PedidoService) {
 
-    this.pedido = this.navParams.get('pedido');
+      this.pedido = this.navParams.get('pedido');
   }
 
   ionViewDidLoad() {
@@ -55,7 +56,7 @@ export class OrderConfirmationPage {
     this.pedidoService.insert(this.pedido)
     .subscribe(response => {
       this.cartService.createOrClearCart();
-      console.log(response.headers.get('location'));
+      this.codPedido = this.extractId(response.headers.get('location'));
     },
     error => {
       if (error.status == 403) {
@@ -66,5 +67,14 @@ export class OrderConfirmationPage {
 
   back() {
     this.navCtrl.setRoot('CartPage');
+  }
+
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
+  }
+
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
   }
 }
